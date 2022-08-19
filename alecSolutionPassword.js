714fc150e8859d5459de9ce7ba60d2fa0d53394a
Gabriel Wilkins <wilkigab001@gmail.com>
2022-08-19 16:07:13 -0600

    Added alecs solution for the password problem so i have additional resources to review

---
+++ alecSolutionPassword.js
@@ -0,0 +1,33 @@
+let password = 'abcdefg123';
+let minChars = false;
+let upperCheck = false;
+let numCheck = false;
+let letterCheck = false;
+
+for(let i = 0; i < password.length; i++){
+    console.log(password[i].toUpperCase())
+    //checking if the character can be converted to a number data type
+    if(!isNaN(+password[i])){
+        numCheck = true;
+    }
+    //checking if number is uppercase
+    if(password[i] === password[i].toUpperCase()){
+        upperCheck = true;
+    }
+}
+//check that the password has a letter
+
+if(/[a-zA-Z]/.test(password)){
+    letterCheck = true;
+}
+//check that password meets minimum and maximum amount of characters
+if(password.length >= 10 && password.length < 20){
+    minChars = true;
+}
+//check that everything is true
+
+if(minChars && upperCheck && numCheck && letterCheck){
+    console.log('success')
+}
+
+console.log(letterCheck)
